f758e398309aafef1a533b0b7a764aef1ff41646
Shak-O <shako.turashvili123@gmail.com>
2026-01-15 10:00:02 +0400

    smol

---
+++ EmployeeWebApp/EmployeeWebApp/Controllers/ContextTestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeWebApp.Controllers;
+
+[ApiController]
+[Route("employees")]
+public class ContextTestController : ControllerBase
+{
+    [HttpPost("test")]
+    public void GetData([FromBody]ContextTestData data)
+    {
+        HttpContext.Response.StatusCode = 200;
+        HttpContext.Response.ContentType = "text/plain";
+        HttpContext.Response.WriteAsync(data.Name + " " + data.LastName).GetAwaiter().GetResult();
+    }
+}
+
+public class ContextTestData
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LastName  { get; set; }
+}
