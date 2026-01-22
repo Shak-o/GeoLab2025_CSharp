c2897ddc1653f281918340f38b60cf32269c548a
Shak-O <shako.turashvili123@gmail.com>
2026-01-22 21:30:01 +0400

    34

--- EmployeeWebApp/EmployeeWebApp/Services/TestSingletonService.cs
+++ EmployeeWebApp/EmployeeWebApp/Services/TestSingletonService.cs
@@ -3,14 +3,29 @@
 public class TestSingletonService
 {
     private Guid _serviceId;
+    private IServiceProvider _serviceProvider;
     
-    public TestSingletonService()
+    public TestSingletonService(TestTransientService transientService, IServiceProvider serviceProvider)
     {
+        _serviceProvider = serviceProvider;
         _serviceId = Guid.NewGuid();
     }
     
     public Guid GetServiceId()
     {
         return _serviceId;
+    }
+
+    public void BackgroundJob()
+    {
+        while (true)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var transientService1 = scope.ServiceProvider.GetRequiredService<TestTransientService>();
+                transientService1.GetServiceId();
+            }
+            Task.Delay(TimeSpan.FromHours(1)).Wait();
+        }
     }
 }
